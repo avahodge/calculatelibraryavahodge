5256074ac7a1323348423664b2316ace245349bc
avahodge <20hodgea15@stu.smuhsd.org>
2018-09-12 10:47:51 -0700

    e

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,8 @@
+//This is the client code for the Calculate Library
+//@author Ava Hodge
+//@version 9-6-18
+public class DoMath {
+	public static void main (String[] args) {
+		System.out.println (Calculate.square (-3));
+	}
+}
---
+++ Calculate Library/src/calculate.java
@@ -0,0 +1,9 @@
+//this class contains the methods to do various math-related tasks
+//@author Ava Hodge
+//@version 9-6-18
+public class calculate {
+//returns the square of an integer
+	public static int square(int number) {
+		return number * number;
+	}
+}
