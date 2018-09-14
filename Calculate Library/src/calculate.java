5df96e29a7027253d8bfc327e3ae9efb086a0980
avahodge <20hodgea15@stu.smuhsd.org>
2018-09-14 09:50:26 -0700

    hhh

--- Calculate Library/src/calculate.java
+++ Calculate Library/src/calculate.java
@@ -9,26 +9,22 @@ public class calculate {
 	public static int cube(int number) {
 		return number * number * number;
 	}
-	public static double average(double numberone, double numbertwo) {
-		return (numberone + numbertwo) / 2;
+	public static double average(double a, double b) {
+		return (a * b) / 2;
 	}
-	public static double average(double numberone, double numbertwo, double numberthree) {
-		return (numberone + numbertwo + numberthree) / 3;	
-		}
-	public static double toDegrees(double numberone) {
-		return (numberone / 3.14159) * 180;
+	public static double toDegrees(double a) {
+		return (a * 180) / 3.14159;
 	}
-	public static double discriminant(double a, double b, double c) {
-		return (b * b) - (4 * (a * c));
+	public static double toRadians(double a) {
+		return (a * 3.14159) / 180;
 	}
-	public static int toImproperFrac (int number1, int number2, int number3) {
-		return (number1 * number3) + number2;
-		return "/" + number3;
+	public static double discriminant (double a, double b, double c) {
+		return (a * a) - (4 * (a * c));
 	}
-	public static int toMixedNum(int number1, int number2) {
-		return number1 / number2;
-		return " " + (number1 % number2);
-		return "/" + number2;
+	public static String toImproperFrac (int a, int b, int c) {
+		int number = ((a * c) + b);
+		String str1 = String.valueOf(number);
+		String str2 = String.valueOf(c);
+		return str1 + "/" + str2;
 	}
-	
 }
