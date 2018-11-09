cbc0e93a37d517095c10651f7321a6eae8f38f73
avahodge <20hodgea15@stu.smuhsd.org>
2018-11-09 09:46:59 -0800

    .

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -4,10 +4,12 @@ public class LotsOfCopies {
 		int num = 7;
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
-		
-		changeMe(num, strMain, arrMain);
-		System.out.println("num:" + num);
-		S
-			
 	}
+
+public static void changeMe(int x, String str) {
+	x = 4;
+	str = "APCS";
+	//int[] arr = {1, 3, 5, 7, 9};
+	System.out.println("String str = " + str);
+ }
 }
